fb229e37992de7fba84398bcd4e4e8455a43903b
Mohamed Hamdy <mohamedhamdy120@hotmail.com>
2026-05-03 13:46:44 +0300

    initialize the project structure and create Post model

---
+++ UnitsApi/Models/Post.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UnitApi.Models;
+
+public class Post
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
